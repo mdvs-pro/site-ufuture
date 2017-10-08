4b8e4c62fa89c0918622479d94f33f6434c9a4ad
yuchiko <iyuchiko@gmail.com>
2017-10-08 23:49:03 +0300

    add closing mobile menu on link with hash clicked

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,14 @@ import Menu from './lib/Menu.js';
 import {smooth} from './lib/smoothscroll.min.js';
 import App from './lib/App.js';
 
+Menu.init();
+App.init();
+
+document.addEventListener("hashLinkClicked", function(e) {
+  let el = document.querySelector('body');
+  if (DomLib.hasClass(el, Menu.activeState)) DomLib.removeClass(el, Menu.activeState)
+});
+
 setTimeout(function(){
   testBodyStatus();
 }, 4000)
@@ -13,6 +21,3 @@ function testBodyStatus(){
   let el = document.querySelector('.out');
   DomLib.triggerClass(el, LOADED_CLASS);
 }
-
-Menu.init();
-App.init();
--- src/js/lib/App.js
+++ src/js/lib/App.js
@@ -4,6 +4,9 @@ let Ufuture = {
   },
   smoothHashLinks: function(){
     let linksArray = document.querySelectorAll('a');
+    let event = document.createEvent("Event");
+    event.initEvent("hashLinkClicked", true, true);
+
     linksArray = Array.from(linksArray);
     linksArray = linksArray.filter(function(link){
       let linkHref = link.getAttribute('href');
@@ -12,7 +15,9 @@ let Ufuture = {
     linksArray.forEach(function(link){
       link.addEventListener('click', function(e){
         e.preventDefault();
-        document.querySelector(link.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });        
+        let linkTarget = document.querySelector(link.getAttribute('href'));
+        link.dispatchEvent(event);
+        window.scroll({ top: linkTarget.offsetTop, left: 0, behavior: 'smooth' });
       });
     });
   }
